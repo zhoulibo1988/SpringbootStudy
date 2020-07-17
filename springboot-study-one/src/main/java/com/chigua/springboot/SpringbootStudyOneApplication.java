f46a5c6d8ea1208ec1cb50bda0e653ea30a3b5fb
zhoulibo <zhoulibo>
2020-07-17 17:43:03 +0800

    add

---
+++ springboot-study-one/src/main/java/com/chigua/springboot/SpringbootStudyOneApplication.java
@@ -0,0 +1,18 @@
+package com.chigua.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 开始学习Springboot 第一天
+ * @return
+ * @author Mr.zhou
+ * @date 2020-07-17
+ */
+@SpringBootApplication
+public class SpringbootStudyOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyOneApplication.class, args);
+	}
+
+}
