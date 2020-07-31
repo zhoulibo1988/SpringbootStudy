5b3bf6639aefe88a700f22d5d1f2df7967c20499
zhoulibo <zhoulibo>
2020-07-31 15:13:46 +0800

    add 异常处理

---
+++ 06.springboot-study-result/src/main/java/com/chigua/springboot/SpringbootStudyResultApplication.java
@@ -0,0 +1,18 @@
+package com.chigua.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Spring Boot 返回结果的统一封装
+ * @return
+ * @author Mr.zhou
+ * @date 2020-07-17
+ */
+@SpringBootApplication
+public class SpringbootStudyResultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyResultApplication.class, args);
+	}
+
+}
