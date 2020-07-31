2b4672a6501dfc6f2c8293db80b628851b177d71
zhoulibo <zhoulibo>
2020-07-31 16:33:49 +0800

    ADD

---
+++ 08.springboot-study-thymeleaf/src/main/java/com/chigua/springboot/SpringbootStudyThymeleafApplication.java
@@ -0,0 +1,18 @@
+package com.chigua.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Spring Boot thymeleaf 模板
+ * @return
+ * @author Mr.zhou
+ * @date 2020-07-17
+ */
+@SpringBootApplication
+public class SpringbootStudyThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyThymeleafApplication.class, args);
+	}
+
+}
